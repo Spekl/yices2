5820776a018078d0dba5b0b3e8f198ed9b0a1d50
John L. Singleton <jsinglet@gmail.com>
2015-08-30 16:07:35 -0400

    Updates to yices2 version 2.4.1-3

--- yices.osx.sh
+++ yices.osx.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 ##
 ## OSX Binaries aren't easy to ship statically linked so we need to work around this...
 ##
-CWD=`pwd`
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 ##
 ## This allows the yices binary to find its libraries inside this package.
 ##
-export DYLD_LIBRARY_PATH=$CWD/yices-2.4.1/lib:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$DIR/yices-2.4.1/lib:$DYLD_LIBRARY_PATH
 
 ##
 ## Actually execute the real prover 
 ##
 
-./yices.bin "$@"
+PATH=$DIR:$PATH
+
+yices.bin "$@"
